eb23613444795a28b87e0270f89d5d16ce965cc4
grab777 <grab777@gmail.com>
2025-05-02 17:39:44 +0200

    add chatgpt promt

--- OpenAI.py
+++ OpenAI.py
@@ -1,8 +1,9 @@
 import openai
-from dotenv import load_dotenv
+from dotenv import dotenv_values
 
-load_dotenv()
-client = openai.OpenAI()
+config = dotenv_values(".env")
+OPENAI_API_KEY = config.get("OPENAI_API_KEY")
+client = openai.OpenAI(api_key=OPENAI_API_KEY)
 
 
 def get_dalle_image(prompt, debug=False):
@@ -25,4 +26,44 @@ def get_dalle_image(prompt, debug=False):
         return image_url, revised_prompt
     except Exception as e:
         print(f"Error generating image: {e}")
+        return None
+
+
+instruction = """Instruction: Please answer the prompt, which follows in the next message. Format your answer in the Telegram Markdown syntax:
+*bold text*
+_italic text_
+[inline URL](http://www.example.com/)
+[inline mention of a user](tg://user?id=123456789)
+`inline fixed-width code`
+```
+pre-formatted fixed-width code block
+```
+```python
+pre-formatted fixed-width code block written in the Python programming language
+```
+"""
+
+
+def get_chatgpt_response(prompt, debug=False):
+    """
+    Generate a response using OpenAI's ChatGPT model based on the provided prompt.
+    """
+
+    try:
+        print(f"Generating response for prompt: {prompt}")
+        response = client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=[
+                {"role": "system", "content": instruction},
+                {"role": "user", "content": prompt},
+            ],
+            max_tokens=1000,
+            temperature=0.7,
+        )
+        if debug:
+            print("Response from OpenAI API:")
+            print(response)
+        return response.choices[0].message.content
+    except Exception as e:
+        print(f"Error generating response: {e}")
         return None
--- main.py
+++ main.py
@@ -1,35 +1,46 @@
 import telebot
-from dotenv import load_dotenv
+from dotenv import dotenv_values
 import os
 import OpenAI
 
-load_dotenv()
-
-telegram_api_key = os.getenv("TELEGRAM_BOT_API_KEY")
-
+config = dotenv_values(".env")
+telegram_api_key = config.get("TELEGRAM_BOT_API_KEY")
 bot = telebot.TeleBot(telegram_api_key)
 print("Started telegram bot")
+
 
 @bot.message_handler(commands=["start", "help"])
 def send_welcome(message):
     bot.reply_to(message, "Howdy, how are you doing?")
 
 
-# @bot.message_handler(func=lambda message: True)
-# def echo_all(message):
-#     print(message)
-#     bot.reply_to(message, message.text)
-
-command = telebot.types.BotCommand("start", "Start the bot")
-
-
-# @bot.set_my_commands([command], scope=telebot.types.BotCommandScopeDefault())
 @bot.message_handler(commands=["chatgpt"])
 def openai(message):
-    bot.reply_to(
-        message,
-        "OpenAI is an AI research and deployment company. Their mission is to ensure that artificial general intelligence (AGI) benefits all of humanity.",
+    ForceReply = telebot.types.ForceReply(selective=True)
+    bot.send_message(
+        message.chat.id,
+        "Please enter your question to ChatGPT",
+        reply_markup=ForceReply,
     )
+    bot.register_next_step_handler(message, process_chatgpt_prompt)
+
+
+def process_chatgpt_prompt(message):
+    response = OpenAI.get_chatgpt_response(message.text)
+    if response is not None:
+        try:
+            bot.send_message(message.chat.id, response, parse_mode="Markdown")
+        except telebot.apihelper.ApiException as e:
+            print(f"Error sending message: {e}")
+            bot.send_message(
+                message.chat.id,
+                "Sorry, I couldn't send the response. Please try again later.",
+            )
+    else:
+        bot.send_message(
+            message.chat.id,
+            "Sorry, I couldn't generate a response. Please try again later.",
+        )
 
 
 @bot.message_handler(commands=["dalle"])
